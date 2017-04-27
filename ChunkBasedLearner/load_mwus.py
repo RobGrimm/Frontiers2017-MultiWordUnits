83d8165cc357cadbbc9ab82a9c8f4249b9f804c7
RobGrimm <robert.grimm@uantwerpen.be>
2017-04-27 13:25:05 +0200

    changed location of multi-word units

--- ChunkBasedLearner/load_mwus.py
+++ ChunkBasedLearner/load_mwus.py
@@ -6,10 +6,10 @@ file_dir = os.path.dirname(os.path.realpath(__file__))
 
 
 def load_mwus(corpus_name):
-    mwus = pickle.load(open(os.path.join(file_dir, 'MWUs', '%s.pickle' % corpus_name), 'rb'))
+    mwus = pickle.load(open(os.path.join(file_dir, '%s.pickle' % corpus_name), 'rb'))
     return mwus
 
 
 def load_random_baseline(corpus_name):
-    baseline = pickle.load(open(os.path.join(file_dir, 'MWUs', '%s_baseline.pickle' % corpus_name), 'rb'))
+    baseline = pickle.load(open(os.path.join(file_dir, '%s_baseline.pickle' % corpus_name), 'rb'))
     return baseline
